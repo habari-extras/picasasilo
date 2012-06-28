f279caf210d5febba4c3db051240418963d25ff0
Konzertheld <christian.gredig@googlemail.com>
2012-06-29 01:06:28 +0200

    Uh sorry, that should have gone to my fork... Initial commit of Konzertheld.de Picasasilo changes (reverse-merged from commit 33f5e6a8d5792fe6702880803552166fb5090516)

--- block.picasa_album.php
+++
@@ -1,4 +0,0 @@
-<?php if(count($post->picasa_images)):
-foreach($post->picasa_images as $image): ?>
-<a href="<?php echo $image['picasa_url']; ?>"><img src="<?php echo $image['url']; ?>" alt="<?php echo $image['title']; ?>"></a>
-<?php endforeach; endif; ?>
--- picasasilo.album.php
+++
@@ -1,5 +0,0 @@
-<?php
-foreach($theme->tvi_picasa_photos as $photo): ?>
-	<p><a href="<?php echo $photo['picasa_url']; ?>"><img src="<?php echo $photo['url']; ?>"></a></p>
-<?php endforeach; ?>
-<a href="<?php echo $theme->tvi_picasa_albumlink; ?>"><?php _e("View all photos"); ?></a>
